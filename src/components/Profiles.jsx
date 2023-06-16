d65a04969dd3cb34782f293575188c829a3090a6
Rogelio <rhidalgo987@gmail.com>
2023-06-16 01:21:24 -0700

    update

--- src/components/Profiles.jsx
+++ src/components/Profiles.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 import ReactToPrint from 'react-to-print';
-import profilesData from '../data/profiles'
-import './profiles.css'
+import profilesData from '../data/profiles';
+import './profiles.css';
 
 const Profiles = () => {
 	const [card, setCard] = useState(false)
